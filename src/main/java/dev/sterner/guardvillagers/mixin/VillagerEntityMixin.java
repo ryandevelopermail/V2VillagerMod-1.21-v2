3bea77246a1fd8df72dc728e77dd9c112eab541a
ryanpc <ryandevelopermail@gmail.com>
2025-12-21 02:42:34 -0500

    Ensure player-spawned fishermen get fishing rods

--- src/main/java/dev/sterner/guardvillagers/mixin/VillagerEntityMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/VillagerEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,16 +20,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(VillagerEntity.class)
 public abstract class VillagerEntityMixin extends MobEntity {
 
+    @Unique
+    private boolean guardvillagers$playerSpawned;
+    @Unique
+    private boolean guardvillagers$gaveRod;
+
     protected VillagerEntityMixin(net.minecraft.entity.EntityType<? extends MobEntity> entityType, net.minecraft.world.World world) {
         super(entityType, world);
     }
 
     @Inject(method = "initialize", at = @At("TAIL"))
     private void guardvillagers$giveFisherRod(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, CallbackInfoReturnable<EntityData> cir) {
-        if (!(world instanceof ServerWorld) || !this.guardvillagers$isPlayerCreated(spawnReason)) {
+        this.guardvillagers$playerSpawned = this.guardvillagers$isPlayerCreated(spawnReason);
+        if (!(world instanceof ServerWorld)) {
             return;
         }
         VillagerEntity villager = (VillagerEntity) (Object) this;
+        this.guardvillagers$tryGiveRod(villager);
+    }
+
+    @Inject(method = "setVillagerData", at = @At("TAIL"))
+    private void guardvillagers$onDataChanged(net.minecraft.village.VillagerData villagerData, CallbackInfoReturnable<net.minecraft.village.VillagerData> cir) {
+        if (!this.getWorld().isClient()) {
+            VillagerEntity villager = (VillagerEntity) (Object) this;
+            this.guardvillagers$tryGiveRod(villager);
+        }
+    }
+
+    @Unique
+    private void guardvillagers$tryGiveRod(VillagerEntity villager) {
+        if (!this.guardvillagers$playerSpawned || this.guardvillagers$gaveRod) {
+            return;
+        }
         if (villager.getVillagerData().getProfession() != net.minecraft.village.VillagerProfession.FISHERMAN) {
             return;
         }
@@ -37,6 +60,7 @@ public abstract class VillagerEntityMixin extends MobEntity {
             villager.equipStack(EquipmentSlot.MAINHAND, fishingRod.copy());
         }
         villager.getInventory().addStack(fishingRod);
+        this.guardvillagers$gaveRod = true;
     }
 
     private boolean guardvillagers$isPlayerCreated(SpawnReason spawnReason) {
