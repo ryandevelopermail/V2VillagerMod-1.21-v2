7c1843e2a8faa89c0477bf4c8d0eaee9fdbc4732
ryanpc <ryandevelopermail@gmail.com>
2025-12-28 23:04:36 -0500

    Add shepherd crafting recipes

---
+++ src/main/java/dev/sterner/guardvillagers/common/villager/behavior/ShepherdBehavior.java
@@ -0,0 +1,37 @@
+package dev.sterner.guardvillagers.common.villager.behavior;
+
+import dev.sterner.guardvillagers.common.entity.goal.ShepherdCraftingGoal;
+import dev.sterner.guardvillagers.common.villager.VillagerProfessionBehavior;
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class ShepherdBehavior implements VillagerProfessionBehavior {
+    private static final int CRAFTING_GOAL_PRIORITY = 4;
+    private static final Map<VillagerEntity, ShepherdCraftingGoal> CRAFTING_GOALS = new WeakHashMap<>();
+
+    @Override
+    public void onChestPaired(ServerWorld world, VillagerEntity villager, BlockPos jobPos, BlockPos chestPos) {
+        ShepherdCraftingGoal craftingGoal = CRAFTING_GOALS.get(villager);
+        if (craftingGoal != null) {
+            craftingGoal.setTargets(jobPos, chestPos, craftingGoal.getCraftingTablePos());
+        }
+    }
+
+    @Override
+    public void onCraftingTablePaired(ServerWorld world, VillagerEntity villager, BlockPos jobPos, BlockPos chestPos, BlockPos craftingTablePos) {
+        ShepherdCraftingGoal goal = CRAFTING_GOALS.get(villager);
+        if (goal == null) {
+            goal = new ShepherdCraftingGoal(villager, jobPos, chestPos, craftingTablePos);
+            CRAFTING_GOALS.put(villager, goal);
+            GoalSelector selector = villager.goalSelector;
+            selector.add(CRAFTING_GOAL_PRIORITY, goal);
+        } else {
+            goal.setTargets(jobPos, chestPos, craftingTablePos);
+        }
+    }
+}
