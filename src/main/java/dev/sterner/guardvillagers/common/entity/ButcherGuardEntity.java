ca3cce04d91222790c0fa11e5924051fd5d83237
ryanpc <ryandevelopermail@gmail.com>
2025-12-29 23:44:10 -0500

    Fix butcher guard mainhand loadout

--- src/main/java/dev/sterner/guardvillagers/common/entity/ButcherGuardEntity.java
+++ src/main/java/dev/sterner/guardvillagers/common/entity/ButcherGuardEntity.java
@@ -1,10 +1,22 @@
 package dev.sterner.guardvillagers.common.entity;
 
+import java.util.List;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 
 public class ButcherGuardEntity extends GuardEntity {
     public ButcherGuardEntity(EntityType<? extends GuardEntity> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    public List<ItemStack> getStacksFromLootTable(EquipmentSlot slot, ServerWorld serverWorld) {
+        if (slot == EquipmentSlot.MAINHAND) {
+            return List.of();
+        }
+        return super.getStacksFromLootTable(slot, serverWorld);
     }
 }
