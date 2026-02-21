70c9a0b141edd52efd099cc4bb075e8eac1221a1
ryanpc <ryandevelopermail@gmail.com>
2026-02-20 20:58:13 -0500

    Use template bell infos for village chest placement

--- src/main/java/dev/sterner/guardvillagers/mixin/StructureTemplateMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/StructureTemplateMixin.java
@@ -8,13 +8,18 @@ import net.minecraft.structure.StructureTemplate;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.ServerWorldAccess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(StructureTemplate.class)
 public class StructureTemplateMixin {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StructureTemplateMixin.class);
 
     @Inject(method = "place(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/StructurePlacementData;Lnet/minecraft/util/math/random/Random;I)Z", at = @At("RETURN"))
     private void guardvillagers$placeVillageChestNearBell(ServerWorldAccess world, BlockPos pos, BlockPos pivot, StructurePlacementData placementData, Random random, int flags, CallbackInfoReturnable<Boolean> cir) {
@@ -22,14 +27,15 @@ public class StructureTemplateMixin {
             return;
         }
 
-        int radius = 6;
-        for (BlockPos checkPos : BlockPos.iterate(pos.add(-radius, -3, -radius), pos.add(radius, 3, radius))) {
-            BlockState blockState = world.getBlockState(checkPos);
-            if (!blockState.isOf(Blocks.BELL)) {
-                continue;
-            }
+        StructureTemplate structureTemplate = (StructureTemplate) (Object) this;
+        List<StructureTemplate.StructureBlockInfo> bells = structureTemplate.getInfosForBlock(pos, placementData, Blocks.BELL);
+        LOGGER.debug("Structure placement at {} resolved {} bell(s) in template output", pos.toShortString(), bells.size());
 
-            VillageBellChestPlacementHelper.tryPlaceChestForVillageBell(world, checkPos.toImmutable(), blockState, placementData);
+        for (StructureTemplate.StructureBlockInfo bellInfo : bells) {
+            BlockPos bellPos = bellInfo.pos().toImmutable();
+            BlockState bellState = bellInfo.state();
+            LOGGER.debug("Passing resolved village bell {} to chest placement helper", bellPos.toShortString());
+            VillageBellChestPlacementHelper.tryPlaceChestForVillageBell(world, bellPos, bellState, placementData);
         }
     }
 }
