bcc93067078e7257f7184e542adc8cc77f7735a9
ryanpc <ryandevelopermail@gmail.com>
2026-02-20 20:37:10 -0500

    Fix StructureTemplate mixin signature and reduce scan cost

--- src/main/java/dev/sterner/guardvillagers/mixin/StructureTemplateMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/StructureTemplateMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.structure.StructureTemplate;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.ServerWorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,13 +16,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(StructureTemplate.class)
 public class StructureTemplateMixin {
 
-    @Inject(method = "place(Lnet/minecraft/world/StructureWorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/StructurePlacementData;Lnet/minecraft/util/math/random/Random;I)Z", at = @At("RETURN"))
-    private void guardvillagers$placeVillageChestNearBell(StructureWorldAccess world, BlockPos pos, BlockPos pivot, StructurePlacementData placementData, Random random, int flags, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "place(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/StructurePlacementData;Lnet/minecraft/util/math/random/Random;I)Z", at = @At("RETURN"))
+    private void guardvillagers$placeVillageChestNearBell(ServerWorldAccess world, BlockPos pos, BlockPos pivot, StructurePlacementData placementData, Random random, int flags, CallbackInfoReturnable<Boolean> cir) {
         if (!cir.getReturnValueZ()) {
             return;
         }
 
-        int radius = 12;
+        int radius = 6;
         for (BlockPos checkPos : BlockPos.iterate(pos.add(-radius, -3, -radius), pos.add(radius, 3, radius))) {
             BlockState blockState = world.getBlockState(checkPos);
             if (!blockState.isOf(Blocks.BELL)) {
