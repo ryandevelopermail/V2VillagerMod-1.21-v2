d4692b49f240be306c9a31810ed2fcdf1699cdf1
ryanpc <ryandevelopermail@gmail.com>
2026-02-24 23:50:33 -0500

    Fix chest mixin injection targets for inventory mutations

--- src/main/java/dev/sterner/guardvillagers/mixin/ChestBlockEntityMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/ChestBlockEntityMixin.java
@@ -3,17 +3,34 @@ package dev.sterner.guardvillagers.mixin;
 import dev.sterner.guardvillagers.common.villager.behavior.ChestInventoryChangeDispatcher;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChestBlockEntity.class)
 public abstract class ChestBlockEntityMixin {
-    @Inject(method = "markDirty", at = @At("TAIL"))
-    private void guardvillagers$notifyShepherdListeners(CallbackInfo ci) {
+    @Inject(method = {
+            "setStack(ILnet/minecraft/item/ItemStack;)V",
+            "clear()V"
+    }, at = @At("TAIL"))
+    private void guardvillagers$notifyShepherdListenersOnVoidMutation(CallbackInfo ci) {
+        guardvillagers$notifyShepherdListeners();
+    }
+
+    @Inject(method = {
+            "removeStack(II)Lnet/minecraft/item/ItemStack;",
+            "removeStack(I)Lnet/minecraft/item/ItemStack;"
+    }, at = @At("TAIL"))
+    private void guardvillagers$notifyShepherdListenersOnReturnMutation(CallbackInfoReturnable<ItemStack> cir) {
+        guardvillagers$notifyShepherdListeners();
+    }
+
+    private void guardvillagers$notifyShepherdListeners() {
         BlockEntity self = (BlockEntity) (Object) this;
         World world = self.getWorld();
         if (world instanceof ServerWorld serverWorld) {
