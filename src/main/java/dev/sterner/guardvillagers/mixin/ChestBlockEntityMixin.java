9c75ebc1d14f46137f34f5474f7edd34b73874a0
ryanpc <ryandevelopermail@gmail.com>
2026-02-25 20:44:54 -0500

    Fix chest mixin by hooking markDirty on ChestBlockEntity

--- src/main/java/dev/sterner/guardvillagers/mixin/ChestBlockEntityMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/ChestBlockEntityMixin.java
@@ -2,8 +2,6 @@ package dev.sterner.guardvillagers.mixin;
 
 import dev.sterner.guardvillagers.common.villager.behavior.ShepherdBehavior;
 import net.minecraft.block.entity.ChestBlockEntity;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -11,34 +9,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LootableContainerBlockEntity.class)
+@Mixin(ChestBlockEntity.class)
 public abstract class ChestBlockEntityMixin {
-    @Inject(method = "setStack", at = @At("TAIL"))
-    private void guardvillagers$notifyShepherdGoalsOnSetStack(int slot, ItemStack stack, CallbackInfo ci) {
-        guardvillagers$notifyShepherdGoalsOnChestMutation();
-    }
-
-    @Inject(method = "removeStack(II)Lnet/minecraft/item/ItemStack;", at = @At("TAIL"))
-    private void guardvillagers$notifyShepherdGoalsOnSplitRemove(int slot, int amount, CallbackInfoReturnable<ItemStack> cir) {
-        guardvillagers$notifyShepherdGoalsOnChestMutation();
-    }
-
-    @Inject(method = "removeStack(I)Lnet/minecraft/item/ItemStack;", at = @At("TAIL"))
-    private void guardvillagers$notifyShepherdGoalsOnRemove(int slot, CallbackInfoReturnable<ItemStack> cir) {
-        guardvillagers$notifyShepherdGoalsOnChestMutation();
-    }
-
-    @Inject(method = "clear", at = @At("TAIL"))
-    private void guardvillagers$notifyShepherdGoalsOnClear(CallbackInfo ci) {
-        guardvillagers$notifyShepherdGoalsOnChestMutation();
-    }
-
-    private void guardvillagers$notifyShepherdGoalsOnChestMutation() {
-        if (!((Object) this instanceof ChestBlockEntity chest)) {
-            return;
-        }
+    @Inject(method = "markDirty", at = @At("TAIL"))
+    private void guardvillagers$notifyShepherdGoalsOnChestMutation(CallbackInfo ci) {
+        ChestBlockEntity chest = (ChestBlockEntity) (Object) this;
         World world = chest.getWorld();
         if (!(world instanceof ServerWorld serverWorld)) {
             return;
