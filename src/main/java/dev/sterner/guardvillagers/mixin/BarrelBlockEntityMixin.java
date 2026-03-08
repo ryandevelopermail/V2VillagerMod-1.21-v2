79e3933a87e7ebb39a4e962bcf6a525443620975
ryanpc <ryandevelopermail@gmail.com>
2026-03-08 01:30:19 -0500

    Retarget barrel markDirty mixin to BlockEntity

--- src/main/java/dev/sterner/guardvillagers/mixin/BarrelBlockEntityMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/BarrelBlockEntityMixin.java
@@ -1,18 +1,18 @@
 package dev.sterner.guardvillagers.mixin;
 
 import dev.sterner.guardvillagers.common.villager.behavior.FishermanBehavior;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BarrelBlockEntity;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LootableContainerBlockEntity.class)
+@Mixin(BlockEntity.class)
 public class BarrelBlockEntityMixin {
 
-    @Inject(method = "markDirty", at = @At("TAIL"), require = 0)
+    @Inject(method = "markDirty()V", at = @At("TAIL"), require = 0)
     private void guardvillagers$onBarrelInventoryChanged(CallbackInfo ci) {
         if ((Object) this instanceof BarrelBlockEntity barrel && barrel.getWorld() instanceof ServerWorld serverWorld) {
             FishermanBehavior.onBarrelInventoryMutated(serverWorld, barrel.getPos());
